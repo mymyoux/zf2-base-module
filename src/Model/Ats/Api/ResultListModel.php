2f886076821c7e70cb25b614a80b3f6302cd07f6
ascheron <benjamin.andreosso@gmail.com>
2016-05-03 14:51:52 +0200

    remvoe resultlitmodel ats from application: move to core

---
+++ src/Model/Ats/Api/ResultListModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Core\Model\Ats\Api;
+use Core\Model\CoreModel;
+
+class ResultListModel extends CoreModel
+{
+	public $count;
+	public $content;
+
+	public function setTotalFound( $count )
+	{
+		$this->count = $count;
+
+		return $this;
+	}
+
+	public function setContent( $content )
+	{
+		$this->content = $content;
+
+		return $this;
+	}
+
+	public function getTotalFound()
+	{
+		return $this->count;
+	}
+
+	public function getContent()
+	{
+		return $this->content;
+	}
+}
