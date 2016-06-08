d223cb86ef26c2e9eb9c6de9051f9c14738c7660
mymyoux <mymyoux@gmail.com>
2016-06-08 11:59:35 +0200

    add toArray

--- src/Core/PseudoTrait/PseudoTrait.php
+++ src/Core/PseudoTrait/PseudoTrait.php
@@ -17,5 +17,47 @@ abstract class PseudoTrait
 	{
 		$this->subject = $subject;
 	}  
-	
+	public function toArray()
+	{
+        $data = to_array($this, $this);
+        return $data;
+        if(!isset($value))
+        {
+            $value = $this;
+        }
+        $keys = get_class_vars(get_class($value));
+        $data = array();
+        foreach($keys as $key => $value)
+        {
+            if(!starts_with($key, "_"))
+            {
+                $data[$key] = $this->$key;
+                if(is_array($this->$key))
+                {
+                    foreach($this->$keys as $k=>$v)
+                    {
+                        if(isset($v))
+                        {
+                            $data[$key][$k] = $this->toArray($v);
+                        }
+                    }
+                }else
+                {
+                    if(is_object($this->$key))
+                    {
+                        if(method_exists($this->$key,"toArray"))
+                        {
+                            $data[$key] = $this->$key->toArray();
+                        }
+                    }
+                }
+            }
+        }
+        if(property_exists($this, "id"))
+        {
+
+            $data["id_".$this->getShortName()] = $this->id;
+        }
+        return $data;
+	}
 } 
