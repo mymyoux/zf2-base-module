2dfed2ee5b8a3f5bef54904c952e7ca462029570
mymyoux <mymyoux@gmail.com>
2016-11-22 12:03:19 +0100

    add cron

---
+++ src/Model/Cron/LogModel.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeremy.dubois
+ * Date: 27/09/2014
+ * Time: 15:21
+ */
+
+namespace Core\Model\Cron;
+use Core\Model\CoreModel;
+
+class LogModel extends CoreModel
+{
+	protected $log_id;
+	public $status;
+	public $ram;
+	public $load;
+	public $execution_time;
+	public $errors;
+	public $critical;
+	// public $critical_message;
+
+	public function getId()
+	{
+		return $this->log_id;
+	}
+
+	public function getStatus()
+	{
+		return $this->status;
+	}
+
+	public function setStatus( $status )
+	{
+		$this->status = $status;
+
+		return $this;
+	}
+
+	public function setRam( $ram )
+	{
+		$this->ram = $ram;
+
+		return $this;
+	}
+
+	public function setLoad( $load )
+	{
+		$this->load = $load;
+
+		return $this;
+	}
+
+	public function setExecutionTime( $execution_time )
+	{
+		$this->execution_time = $execution_time;
+
+		return $this;
+	}
+
+	public function setErrors( $errors )
+	{
+		$this->errors = $errors;
+
+		return $this;
+	}
+
+	public function setApiCall( $api_call )
+	{
+		$this->api_call = $api_call;
+
+		return $this;
+	}
+
+	public function setApiCallError( $api_call_error )
+	{
+		$this->api_call_error = $api_call_error;
+
+		return $this;
+	}
+
+	public function setApiBatch( $api_batch )
+	{
+		$this->api_batch = $api_batch;
+
+		return $this;
+	}
+
+	public function setCriticals( $critical )
+	{
+		$this->critical = $critical;
+
+		return $this;
+	}
+
+	public function setCriticalMessage( $critical_message )
+	{
+		$this->critical_message = $critical_message;
+
+		return $this;
+	}
+}
