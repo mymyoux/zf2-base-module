98668420a494fdd3e1e87b3604e7763239de440a
mymyoux <mymyoux@gmail.com>
2016-12-12 18:27:49 +0100

    default return

--- src/Service/API.php
+++ src/Service/API.php
@@ -460,7 +460,10 @@ class API extends \Core\Service\CoreService implements ServiceLocatorAwareInterf
         {
             $result = array($result_name=>$result);
         }
-
+        if (!isset($result[$result_name]))
+        {
+            $result = [$result_name=>$result];
+        }
         $formatted_result = new \StdClass();
         $formatted_result->value = $result;
         $api_data = new \StdClass();
