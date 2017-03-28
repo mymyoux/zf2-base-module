c700f74404d417bc2aa55150f62e5a2608d78ce4
ascheron <benjamin.andreosso@gmail.com>
2017-03-28 18:42:26 +0200

    log usage

--- src/Service/DetectLanguage.php
+++ src/Service/DetectLanguage.php
@@ -83,14 +83,20 @@ class DetectLanguage extends \Core\Service\CoreService implements ServiceLocator
     }
     protected function _detect($text, $smart)
     {
+        $this->sm->get('Log')->normal($text);
         $usage = $this->getDetectLanguageTable()->getTodayUsage($this->key);
         $remaining_calls = $this->plan["requests"] - (int)$usage["used_calls"];
         $remaining_bytes = $this->plan["bytes"] - (int)$usage["used_bytes"];
+
+        $this->sm->get('Log')->normal('remaining_calls:' . $remaining_calls);
+        $this->sm->get('Log')->normal('remaining_bytes:' . $remaining_bytes);
+        
         if($remaining_calls % 100 === 0)
         {
             $this->getNotificationManager()->alert("detect_language", $remaining_calls." remaining calls - ".intval($remaining_bytes/1024)." ko");
         }
         $size_batch = $remaining_calls>0?$remaining_bytes/$remaining_calls:0;
+        $this->sm->get('Log')->normal('size_batch:' . $size_batch);
         if($size_batch<= 0 )
         {
 
@@ -120,39 +126,48 @@ class DetectLanguage extends \Core\Service\CoreService implements ServiceLocator
 
 
         $count = 1;
-
         do
         {
             $max_size = intval($remaining_bytes/$remaining_calls);
+            $this->sm->get('Log')->normal('max_size:' . $max_size);
+            
             $size = ceil($max_size*$count*3/4);
-
+            $this->sm->get('Log')->normal('size:' . $size);
             if($size>= strlen($text))
             {
+                $this->sm->get('Log')->normal('1/');
                 $cutText = $text;
             }else
             {
+                $this->sm->get('Log')->normal('2/');
                 $position = mb_strrpos($text, " ", -(mb_strlen($text) - $size));
                 if($position === False)
                 {
+                    $this->sm->get('Log')->normal('3/');
                     $position = $size;
                 }
                 $cutText = mb_substr($text, 0, $position);
             }
             if(strlen($cutText)>$remaining_bytes)
             {
+                $this->sm->get('Log')->normal('4/');
                 $cutText = substr($cutText, 0, $remaining_bytes);
                 while(!mb_check_encoding($cutText) && strlen($cutText)>0)
                 {
+                 $this->sm->get('Log')->normal('5/');
                     $cutText = substr($cutText, 0, -1);
                 }
 
                 $position = mb_strrpos($cutText, " ");
                 if($position !== False)
                 {
+                    $this->sm->get('Log')->normal('6/');
                     $cutText = mb_substr($cutText, 0, $position);
                 }
             }
 
+
+            $this->sm->get('Log')->normal($cutText);
 
             //TODO increase the size part by part and save the total calls
             $languages = $this->__getDetection( $cutText );
